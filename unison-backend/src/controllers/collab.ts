17cf92607fb82a3a4460adef2ed14e137a08ce1f
tDev <95761247+tDev0809@users.noreply.github.com>
2023-05-12 21:48:46 -0700

    Update collab.ts

--- unison-backend/src/controllers/collab.ts
+++ unison-backend/src/controllers/collab.ts
@@ -598,7 +598,9 @@ export const updateCollab = async (req: Request, res: Response) => {
       collabitem.expiretimemin * 60 * 1000
   );
   const inprogressdate =
-    (format == 3 ? enddate.valueOf() : new Date().valueOf()) + 3600 * 1000 * 24;
+    (format == 3 ? enddate.valueOf() : new Date().valueOf()) + 1000 * 60 * 5;
+    // const inprogressdate =
+    // (format == 3 ? enddate.valueOf() : new Date().valueOf()) + 3600 * 1000 * 24;
 
   const up: any = await (
     await Collabs.findByIdAndUpdate(_id, {
