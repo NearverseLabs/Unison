8bb35d4b8d9f104b458434d6c84368f8ecde1ee7
tDev <95761247+tDev0809@users.noreply.github.com>
2023-05-13 07:29:15 -0700

    update

--- unison-backend/src/controllers/bot.ts
+++ unison-backend/src/controllers/bot.ts
@@ -29,7 +29,7 @@ const client = new Client({
 // const TOKEN = "MTA5MjcyODc1NjEwOTU5NDY3NA.G1lVgK.fFxYDGNGnuTKOkGBOKyaePO68nJpn27AEaRQrU"
 // const CLIENT_ID = "1092728756109594674"
 const TOKEN = "MTEwMTg0NjUxNTUyMzI3MjczNQ.Gv34iC.TstxkXKa_-MpGHJe_xBHWgTdAU_9MyWEoJ7BZw";
-const CLIENT_ID = "1101846515523272735";
+const CLIENT_ID = "1106950196212007106";
 
 export const connectBOT = async () => {
   client.on(Events.ClientReady, async () => {
